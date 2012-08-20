a781d494e204ed4d53bae23d30ef10159149ae25
Gael Pasgrimaud <gael@gawel.org>
2012-08-20 16:43:12 +0200

    use status_int

--- wsgithumb/django_view.py
+++ wsgithumb/django_view.py
@@ -12,6 +12,7 @@ import os
 
 IMAGE_SIZES = getattr(settings, 'IMAGE_SIZES', DEFAULT_SIZES)
 CACHE_DIRECTORY = getattr(settings, 'CACHE_DIRECTORY', None)
+ACCEL_HEADER = getattr(settings, 'ACCEL_HEADER', False)
 
 if not CACHE_DIRECTORY:
     CACHE_DIRECTORY = os.path.join(tempfile.gettempdir(), 'wsgithumb-cache')
@@ -36,20 +37,19 @@ def serve(request, size, path, factor=100,
         return HttpResponseBadRequest()
 
     size = IMAGE_SIZES[size]
-    accel_header = getattr(settings, 'ACCEL_HEADER', False)
     thumb = get_image_response(
         document_root=document_root,
         cache_directory=cache_directory,
         size=size,
         factor=factor,
         path=path,
-        accel_header=accel_header
+        accel_header=ACCEL_HEADER,
     )
     if thumb.status_int > 400:
-        response = HttpResponse(status=str(thumb.status_int))
+        response = HttpResponse(status=thumb.status_int)
     else:
         response = HttpResponse(thumb.app_iter,
-                                status=str(thumb.status_int),
+                                status=thumb.status_int,
                                 content_type=thumb.content_type)
         for key, value in thumb.headers.iteritems():
             response[key] = value
